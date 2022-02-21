fb32aef3eb9c65965d4fc48982dc63c1701b5859
Kiryha <kiryha@gmail.com>
2022-02-21 15:32:27 -0500

    wip

--- P3/boundedBuffer.c
+++ P3/boundedBuffer.c
@@ -1,17 +1,3 @@
-/*
- * boundedBuffer.c
- *
- * A complete example of simple producer/consumer program. The Producer
- * and Consumer functions are executed as independent threads.  They
- * share access to a single buffer, data.  The producer deposits a sequence
- * of integers from 1 to numIters into the buffer.  The Consumer fetches
- * these values and adds them.  Two semaphores,empty and full are used to
- * ensure that the producer and consumer alternate access to the buffer.
- *
- * SOURCE: adapted from example code in "Multithreaded, Parallel, and
- *         Distributed Programming" by Gregory R. Andrews.
- */
-
 #include <pthread.h>
 #include <semaphore.h>
 #include <stdio.h>
@@ -22,53 +8,77 @@
 void *Producer (void *); // the two threads
 void *Consumer (void *);
 
-sem_t empty, full;       //global semaphores
-int data;                // shared buffer, size = 1
-int numIters;
+// Semaphores
+sem_t empty;
+sem_t full;
+sem_t mutex;
+// Shared buffer of size = 1
+int data;
+// Number of iterations
+int iterations;
 
-// main() -- read command line and create threads
+// Read command line and create threads
 int main(int argc, char *argv[]) {
-    pthread_t pid, cid;
 
-    sem_init(&empty, SHARED, 1);    // sem empty = 1
-    sem_init(&full, SHARED, 0); //sem full = 0
+    // Producers and consumers ids
+    pthread_t producer_id_1;
+    pthread_t producer_id_2;
+    pthread_t producer_id_3;
+    pthread_t consumer_id_1;
+    pthread_t consumer_id_2;
+    pthread_t consumer_id_3;
 
-    if (argc < 2) {
-	    printf("Usage: boundedBuffer <Number of Iterations>\n");
-	    printf(" e.g.: ./boundedBuffer 100 \n"); 
+    sem_init(&empty, SHARED, 1);  // sem empty = 1
+    sem_init(&full, SHARED, 0);   // sem full = 0
+    sem_init(&mutex, SHARED, 1);  // sem mutex = 1
+
+    if (argc < 3) {
+	    printf("Usage: boundedBuffer <number of iterations> <buffer size>\n");
+	    printf("Example: ./boundedBuffer 100 10\n");
 	    exit(0);
     }
-    numIters = atoi(argv[1]);
 
-    pthread_create(&pid, NULL, Producer, NULL);
-    pthread_create(&cid, NULL, Consumer, NULL);
+    iterations = atoi(argv[1]);
+    int buffer_size = atoi(argv[2]);
+    int data_array[buffer_size];
 
-    pthread_join(pid, NULL);
-    pthread_join(cid, NULL);
+    // Create producers and consumers
+    pthread_create(&producer_id_1, NULL, Producer, NULL);
+//    pthread_create(&producer_id_2, NULL, Producer, NULL);
+//    pthread_create(&producer_id_3, NULL, Producer, NULL);
+    pthread_create(&consumer_id_1, NULL, Consumer, NULL);
+//    pthread_create(&consumer_id_2, NULL, Consumer, NULL);
+//    pthread_create(&consumer_id_3, NULL, Consumer, NULL);
+
+    pthread_join(producer_id_1, NULL);
+    pthread_join(consumer_id_1, NULL);
     pthread_exit(0);
 }
 
-// deposit 1, ..., numIters into the data buffer
+// Deposit n iterations into the data buffer
 void *Producer(void *arg) {
     int produced;
 
-    for (produced = 0; produced < numIters; produced++) {
+    for (produced = 0; produced < iterations; produced++) {
         sem_wait(&empty);
         data = produced;
+        printf(">> Produced %d\n", produced);
         sem_post(&full);
     }
+
 }
 
-//fetch numIters items from the buffer and sum them
+// Fetch iterations items from the buffer and sum them
 void *Consumer(void *arg) {
     int total = 0;
     int consumed;
 
-    for (consumed = 0; consumed < numIters; consumed++) {
+    for (consumed = 0; consumed < iterations; consumed++) {
         sem_wait(&full);
         total = total + data;
+        printf(">> Consumed %d\n", consumed);
         sem_post(&empty);
     }
 
-    printf("the total is %d\n", total);
+    printf("The total number is %d\n", total);
 }
